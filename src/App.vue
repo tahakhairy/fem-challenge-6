467445442c6826f8c248bc0b79783ad874b3ad98
tahakhairy <tahakhairy@ymail.com>
2024-01-19 14:39:44 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="container">
+    <div class="card-bg"></div>
+
+    <div class="content">
+      <div class="cards-container">
+        <div class="card-front">
+          <img src="./assets/images/card-logo.svg" alt="card-logo" />
+          <div class="card-info">
+            <div class="card-number">
+              <span>0000</span>
+              <span>0000</span>
+              <span>0000</span>
+              <span>0000</span>
+            </div>
+            <div class="card-name-date">
+              <p>TAHA KHAIRY</p>
+              <p>01/25</p>
+            </div>
+          </div>
+        </div>
+        <div class="card-back">
+          <span class="card-cvv">000</span>
+        </div>
+      </div>
+
+      <div class="card-form">
+        <card-form></card-form>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import CardForm from "@/components/CardForm.vue";
+</script>
+<style scoped>
+.container {
+  height: 100vh;
+  overflow: hidden;
+  max-width: 1900px;
+  display: flex;
+  z-index: 1;
+}
+
+.container .card-bg {
+  position: absolute;
+  width: 30%;
+  background: url(./assets/images/bg-main-desktop.png) no-repeat;
+  height: 100%;
+  z-index: -1;
+}
+
+.container .content {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+}
+
+.container .content .cards-container {
+  margin: 8rem 2rem;
+  width: 447px;
+  height: 245px;
+  display: flex;
+  flex-direction: column;
+  align-self: normal;
+}
+
+.cards-container .card-back {
+  margin-top: 2rem;
+  margin-left: 6rem;
+}
+.cards-container .card-front,
+.cards-container .card-back {
+  width: 100%;
+  height: 100%;
+}
+.cards-container .card-front {
+  background: url(./assets/images/bg-card-front.png);
+  padding: 1.5rem;
+  border-radius: 10px;
+}
+
+.cards-container .card-back {
+  background: url(./assets/images/bg-card-back.png);
+  padding: 1.5rem;
+  border-radius: 10px;
+}
+
+.card-front .card-info {
+  display: flex;
+  flex-direction: column;
+  margin-top: 1rem;
+}
+
+.card-front .card-info .card-number {
+  font-size: 2rem;
+  color: #dedddf;
+  margin: 2rem 0;
+}
+.card-front .card-info .card-number span {
+  margin-right: 0.7rem;
+}
+
+.card-front .card-info .card-name-date {
+  display: flex;
+  justify-content: space-between;
+  color: #dedddf;
+}
+
+.card-back {
+  display: flex;
+  justify-content: end;
+  align-items: center;
+}
+
+.card-back .card-cvv {
+  margin: 6rem 2rem 6rem 6rem;
+  padding-bottom: 0.2rem;
+  color: #dedddf;
+}
+
+.card-form {
+  /* width: 60%; */
+  padding: 8rem;
+}
+</style>
---
+++ src/components/CardForm.vue
@@ -0,0 +1,78 @@
+<template>
+  <form action="">
+    <div class="input">
+      <label for="cardholder-name">CARDHOLDER NAME</label>
+      <input
+        type="text"
+        id="cardholder-name"
+        placeholder="e.g. Jane Appleseed"
+      />
+    </div>
+    <div class="input">
+      <label for="card-number">CARD NUMBER</label>
+      <input
+        type="text"
+        id="card-number"
+        placeholder="e.g. 1234 5678 9123 0000"
+      />
+    </div>
+
+    <div class="card-date-cvc">
+      <div>
+        <label for="card-date">EXP. DATE (MM/YY)</label>
+        <div style="display: inline-flex">
+          <input type="text" placeholder="MM" />
+          <input type="text" placeholder="YY" />
+        </div>
+      </div>
+      <div>
+        <label for="card-cvc">CVC</label>
+        <div>
+          <input type="text" placeholder="CVC" />
+        </div>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script setup></script>
+
+<style scoped>
+form {
+  /* padding: 8rem; */
+  flex: 29%;
+  display: flex;
+  flex-direction: column;
+}
+
+form label {
+  color: hsl(278, 68%, 11%);
+  font-size: 14px;
+  margin-bottom: 0.5rem;
+}
+
+form input {
+  border: none;
+  border-radius: 6px;
+  font-size: 1rem;
+  padding: 0.7rem 1rem;
+}
+
+form div.input {
+  display: flex;
+  flex-direction: column;
+  margin-top: 1.5rem;
+}
+
+form .card-date-cvc {
+  /* width: 100%; */
+  display: flex;
+  flex-direction: row;
+  margin-top: 1.5rem;
+  justify-content: center;
+}
+
+::placeholder {
+  color: hsl(279, 6%, 55%);
+}
+</style>
